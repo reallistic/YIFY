29e57679e68171ea23dde6b921bb610396973d62
rxsegrxup <m.c.chase@spartans.nsu.edu>
2014-06-24 15:48:50 -0400

    Update import.py

--- import.py
+++ import.py
@@ -22,9 +22,9 @@ c = conn.cursor()
 
 #import all items in [movies] into yify.db
 page = 1
+newlist = []
+oldlist = []
 for x in xrange(1, movieCount, LIMIT):	# ie. page in xrange(1, 2388, 50)
-	newlist = False
-	oldlist = []
 	for movie in movies['MovieList']:
 		c.execute("select count(MovieID) from movies where MovieID = :id", {'id':movie['MovieID']})
 		values = (movie['DateUploaded'], movie['DateUploadedEpoch'], movie['MovieID'], 
@@ -32,7 +32,6 @@ for x in xrange(1, movieCount, LIMIT):	# ie. page in xrange(1, 2388, 50)
 					movie['Quality'], movie['ImdbCode'], movie['TorrentUrl'], movie['TorrentHash'], 
 					movie['TorrentMagnetUrl'])
 		if 0 == c.fetchone()[0]:
-			newlist = []
 			newlist.append(movie['MovieTitle'].encode('ascii','ignore'))
 			# record release
 			c.execute('''
@@ -45,20 +44,19 @@ for x in xrange(1, movieCount, LIMIT):	# ie. page in xrange(1, 2388, 50)
 		else:
 			oldlist.append(movie['MovieTitle'].encode('ascii','ignore'))
 
-	logging.info("New releases: %i" % (len(newlist) if newlist else 0))
-	logging.debug("New releases:\n%s" % newlist)
-	logging.info("Old releases: %i" % len(oldlist))
-	logging.debug("Old releases:\n%s" % oldlist)
-	if newlist:
-		MSG = "Fetched:"
-		for x in newlist:
-			MSG += "\n%s" % x
-	else:
-		MSG = "Nothing new released toady"
-	logging.info("Pushover MSG parameter: %s" % MSG)
-	#notify(MSG=MSG)
-
 	page+=1
 	movies = yify.listMovies(quality=QUALITY, set=page)
+
+logging.info("New releases: %i" % (len(newlist) if newlist else 0))
+logging.debug("New releases:\n%s" % newlist)
+logging.info("Old releases: %i" % len(oldlist))
+logging.debug("Old releases:\n%s" % oldlist)
+if len(newlist) > 0:
+	MSG = "Fetched:"
+	for x in newlist:
+		MSG += "\n%s" % x
+else:
+	MSG = "Nothing new released toady"
+logging.info("Pushover MSG parameter: %s" % MSG)
 conn.commit()
 conn.close()
